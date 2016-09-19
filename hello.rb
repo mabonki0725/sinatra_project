7bce63098f74fecda909ad97847541cbeb605983
Masato Nakai <mabonakai@gmail.com>
2016-09-19 05:38:03 +0000

    sinatra add logic

--- hello.rb
+++ hello.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get '/' do 
-  "Hello World xxxx"
+<<EOS
+<html>
+ <head>
+  <title>AIIT Hello</title>
+ </head>
+ <body>
+  <H1>Hellow world, Nakai</H1>
+  こんにちは、これはHerokuで動作するWebアプリです
+ </body>
+</html>
+EOS
+end
+get '/hello/:name' do
+  # "GET /hello/foo" と "GET /hello/bar" にマッチ
+  # params['name'] は 'foo' か 'bar'
+  "Hello #{params['name']}!"
 end
